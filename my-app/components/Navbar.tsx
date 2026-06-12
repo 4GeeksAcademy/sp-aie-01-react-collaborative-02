7669d5818130ec54ddbed960f2a6699f64a434a9
Daniel Rojas Montañez <rojas.daniel.0217@gmail.com>
2026-06-12 19:27:52 +0000

    Creado el Navbar

---
+++ my-app/components/Navbar.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+export default function Navbar () {
+
+    return(
+
+        <h1>
+            <a href=""> Pokedex </a>
+            <a href=""> Moves </a>
+            <a href=""> Species </a>
+            <a href=""> Types </a>
+        </h1>
+
+    )
+
+
+
+}
