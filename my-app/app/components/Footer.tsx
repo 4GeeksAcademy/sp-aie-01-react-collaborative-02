f3d18f35dffbf2e9683f896f538704d972cc13cc
AnthonyPV052 <anthonypelaez052@gmail.com>
2026-06-15 15:54:32 +0000

    Footer añadido

---
+++ my-app/app/components/Footer.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+export default function Footer() {
+
+    return (<div className="text-center p-3">
+        ⭐Created by Daniel, Anthony and Sebástian. 4GeeksAcademy⭐
+    </div>)
+}
