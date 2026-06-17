9f99c54f4fb0847d30fe03dbef9a0f3de97b99ea
Daniel Rojas Montañez <rojas.daniel.0217@gmail.com>
2026-06-17 19:13:20 +0000

    creado parte del pokedex

--- my-app/app/Pokedex/page.tsx
+++ my-app/app/Pokedex/page.tsx
@@ -1,13 +1,48 @@
 import React from "react";
+import { useEffect, useState } from "react";
 
 export default function Pokedex(){
+    const [ pokedex, setPokedex] = useState([])
+
+    	const getPokedex = async () => {
+		const response = await fetch('https://pokeapi.co/api/v2/generation/1')
+		if (!response.ok) {
+			console.log('falso', response.status)
+			return
+		}
+		const data = await response.json()
+		setPokedex(data.pokemon_species)
+
+        	useEffect(() => {
+		    getPokedex()
+
+	}, [])
+
+
+
+	}
+
+
 
 
     return(
 
-        <h1>
-            Pokedex page
-        </h1>
+        <div>
+            {pokedex.map((item, index) =>  
+            
+            <div>    
+                
+
+            </div>)}
+
+
+            
+            
+            
+            
+            }
+
+        </div>
     )
     
 }
